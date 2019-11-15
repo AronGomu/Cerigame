5a99471f35794eca2a7c4390b27ab6e7ea8cc71d
AronGomu <nathan.flachaire@outlook.fr>
2019-11-15 11:49:59 +0100

    quizz finalization

--- CERIGame/app/views/app.js
+++ CERIGame/app/views/app.js
@@ -1,6 +1,6 @@
 var myApp = angular.module('myApp', ['ngRoute']);
 
-var activeUserName = "Rien";
+var activeUserName;
 
 
 myApp.config(function ($routeProvider) {
@@ -25,6 +25,11 @@ myApp.config(function ($routeProvider) {
 
 		.when('/quizz', {
 			templateUrl: 'quizz.html',
+			controller: 'quizzController'
+		})
+
+		.when('/endQuizz', {
+			templateUrl: 'endQuizz.html',
 			controller: 'quizzController'
 		})
 
@@ -67,26 +72,108 @@ myApp.controller('loginController', ['$scope', '$http', '$location', function ($
 
 
 
-myApp.controller('homeController', ['$scope', function ($scope) {
+myApp.controller('homeController', ['$scope', '$location', function ($scope, $location) {
+	if (activeUserName == null) {
+		$location.path('/');
+	}
+
 	$scope.activeUserNameHome = activeUserName;
 }]);
 
 myApp.controller('profilController', ['$scope', function ($scope) {
-	$scope.message = 'Hello profilController!';
+
+	if (activeUserName == null) {
+		$location.path('/');
+	}
 }]);
 
-myApp.controller('quizzController', ['$scope', function ($scope) {
+myApp.controller('quizzController', ['$scope', '$location', function ($scope, $location) {
 
-	$scope.activeQuestion = "This is the question";
+	if (activeUserName == null) {
+		$location.path('/');
+	}
 
-	$scope.listAnswer = [{
-		id: 1,
-		text: "Answer 1"
-	}, {
-		id: 2,
-		text: "Answer 2"
-	}, {
-		id: 3,
-		text: "Answer 3"
-	}]
+	$scope.listQuestions = [{
+			id: 1,
+			text: "This is the question 1",
+			listAnswer: [{
+				id: 1,
+				text: "Answer1 1",
+				is_answer: true
+			}, {
+				id: 2,
+				text: "Answer1 2",
+				is_answer: false
+			}, {
+				id: 3,
+				text: "Answer1 3",
+				is_answer: false
+			}]
+		},
+		{
+			id: 2,
+			text: "This is the question 2",
+			listAnswer: [{
+				id: 1,
+				text: "Answer2 1",
+				is_answer: true
+			}, {
+				id: 2,
+				text: "Answer2 2",
+				is_answer: false
+			}, {
+				id: 3,
+				text: "Answer2 3",
+				is_answer: false
+			}]
+		},
+		{
+			id: 3,
+			text: "This is the question 3",
+			listAnswer: [{
+				id: 1,
+				text: "Answer3 1",
+				is_answer: true
+			}, {
+				id: 2,
+				text: "Answer3 2",
+				is_answer: false
+			}, {
+				id: 3,
+				text: "Answer3 3",
+				is_answer: false
+			}]
+		},
+	]
+
+	$scope.listAnswerOfUser = [];
+
+	$scope.activeQuestion = 1;
+
+	$scope.nbQuestions = $scope.listQuestions.length;
+
+	$scope.nbGoodAnswers = 0;
+
+	$scope.is_end = false;
+
+	$scope.next = function (idAnswer) {
+		$scope.listAnswerOfUser.push(idAnswer);
+		$scope.activeQuestion++;
+		console.log("Valeur de $scope.nbQuestions :" + $scope.nbQuestions);
+		if ($scope.activeQuestion > $scope.listQuestions.length) {
+			$scope.nbGoodAnswers = 0;
+			for (var i = 0; i < $scope.nbQuestions; i++) {
+				console.log("Valeur de i :" + i);
+				console.log("Valeur de $scope.listAnswerOfUser[i] :" + $scope.listAnswerOfUser[i]);
+				console.log("Valeur de $scope.listQuestions[i] :" + $scope.listQuestions[i]);
+				console.log("Valeur de $scope.listQuestions[i].listAnswer[$scope.listAnswerOfUser[i]] :" + $scope.listQuestions[i].listAnswer[$scope.listAnswerOfUser[i]]);
+				console.log("Valeur de $scope.listQuestions[i].listAnswer[$scope.listAnswerOfUser[i]].is_answer :" + $scope.listQuestions[i].listAnswer[$scope.listAnswerOfUser[i] - 1].is_answer);
+				if ($scope.listQuestions[i].listAnswer[$scope.listAnswerOfUser[i] - 1].is_answer == true) {
+					$scope.nbGoodAnswers++;
+				}
+			}
+			$scope.test = "Hello ma boi";
+			$scope.is_end = true;
+		}
+	}
 }]);
